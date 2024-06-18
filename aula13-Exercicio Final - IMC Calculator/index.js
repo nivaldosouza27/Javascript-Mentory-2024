a47e16d54ccc25ca386df126776c77d94a33421d
Nivaldo Souza <nnivaldo_souza@hotmail.com>
2024-06-18 02:55:06 -0300

    Exercicio Objeto e Logica

---
+++ aula13-Exercicio Final - IMC Calculator/index.js
@@ -0,0 +1,55 @@
+function escopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    function recebeEvento(evento){
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        
+        
+        function calculaIMC(peso, altura) {
+            return peso / (altura * altura)
+        }
+        
+        function classificaIMC(imc) {
+            if (imc < 18.5) {
+                return 'Magreza';
+            } else if (imc >= 18.5, imc < 24.9) {
+                return 'Normal';
+            }else if (imc >= 24.9, imc < 29.9) {
+                return 'Sobrepeso';
+            }else if (imc >= 29.9, imc < 39.9) {
+                return 'Obesidade';
+            }else {
+                return 'Obesidade Grave';
+            }
+        }
+
+        const pessoa_objeto = {
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+            imc: calculaIMC(peso.value, altura.value),
+            classificacao: ''
+        };
+
+        pessoa_objeto.classificacao = classificaIMC (pessoa_objeto.imc)
+        
+        resultado.querySelector('.b1').innerHTML = pessoa_objeto.nome + ' ' + pessoa_objeto.sobrenome ;
+        resultado.querySelector('.b2').innerHTML = pessoa_objeto.peso;
+        resultado.querySelector('.b3').innerHTML = pessoa_objeto.altura;
+        resultado.querySelector('.b4').innerHTML = pessoa_objeto.imc.toFixed(2);
+        resultado.querySelector('.b5').innerHTML = pessoa_objeto.classificacao
+    
+        return console.log(pessoa_objeto);
+    }
+
+    form.addEventListener('submit', recebeEvento);
+}
+
+escopo();
---
+++ aula14-Comparacao e Logica/index.js
@@ -0,0 +1,18 @@
+/*
+
+** Operadores de Comparação
+    > Maior que
+    >= Maior que ou igual
+    < Menor que
+    <= Menor que ou igual
+    == Igualdade (valor)
+    === Igualdade (valor e tipo)
+    != Diferença (valor)
+    !== Diferença (valor e tipo)
+
+** Operadores Logicos
+    && -> AND -> E = Todas as comparações precisam ser verdadeiras
+    || -> OR -> OU = Somente uma comparação precisa ser verdadeira
+    ! -> NOT -> NÃO = Negação de uma sentença
+
+*/
